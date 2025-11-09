a7c7fae0a7b9f1df5db60f0e558721d89c6d2852
Huy <buiminhhuyqnam@gmail.com>
2025-11-09 10:02:31 +0700

    fix: missing href

--- src/components/ListPosts.vue
+++ src/components/ListPosts.vue
@@ -21,7 +21,7 @@ function getDate(date: string) {
 function getHref(post: Post) {
   if (post.data.redirect)
     return post.data.redirect
-  return `/posts/${post.slug}`
+  return `posts/${post.slug}`
 }
 
 function getTarget(post: Post) {
